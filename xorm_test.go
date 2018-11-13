2bca42089551aa318fef77c218e83035ce4da8c7
Matti Ranta <matti@mdranta.net>
2018-11-13 15:08:02 -0500

    replace some tests with other types of tests

--- xorm_test.go
+++ xorm_test.go
@@ -6,8 +6,7 @@ import (
 	"fmt"
 
 	"gopkg.in/oauth2.v3/models"
-
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
 	_ "github.com/mattn/go-sqlite3"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
@@ -22,97 +21,55 @@ func TestTokenStore(t *testing.T) {
 	// wait gc
 	defer time.Sleep(time.Second * 2)
 
-	Convey("Test token store", t, func() {
-		store := NewStore(NewConfig(dsn, dbType, ""), 1)
+	assert := assert.New(t)
 
-		Convey("Test authorization code store", func() {
-			info := &models.Token{
-				ClientID:      "1",
-				UserID:        "1_1",
-				RedirectURI:   "http://localhost/",
-				Scope:         "all",
-				Code:          "11_11_11",
-				CodeCreateAt:  time.Now(),
-				CodeExpiresIn: time.Second * 5,
-			}
-			fmt.Println(info)
-			err := store.Create(info)
-			So(err, ShouldBeNil)
+	store := NewStore(NewConfig(dsn, dbType, ""), 1)
 
-			cinfo, err := store.GetByCode(info.Code)
-			So(err, ShouldBeNil)
-			So(cinfo.GetUserID(), ShouldEqual, info.UserID)
+	// "Test authorization code store"
+	info := &models.Token{
+		ClientID:      "1",
+		UserID:        "1_1",
+		RedirectURI:   "http://localhost/",
+		Scope:         "all",
+		Code:          "11_11_11",
+		CodeCreateAt:  time.Now(),
+		CodeExpiresIn: time.Second * 5,
+	}
 
-			err = store.RemoveByCode(info.Code)
-			So(err, ShouldBeNil)
+	err := store.Create(info)
+	assert.Nil(err)
 
-			cinfo, err = store.GetByCode(info.Code)
-			So(err, ShouldBeNil)
-			So(cinfo, ShouldBeNil)
-		})
+	cinfo, err := store.GetByCode(info.Code)
+	assert.Nil(err)
+	assert.Equal(cinfo.GetUserID(), info.UserID, "they should be equal")
 
-		Convey("Test access token store", func() {
-			info := &models.Token{
-				ClientID:        "1",
-				UserID:          "1_1",
-				RedirectURI:     "http://localhost/",
-				Scope:           "all",
-				Access:          "1_1_1",
-				AccessCreateAt:  time.Now(),
-				AccessExpiresIn: time.Second * 5,
-			}
-			err := store.Create(info)
-			So(err, ShouldBeNil)
+	err = store.RemoveByCode(info.Code)
+	assert.Nil(err)
+	cinfo, err = store.GetByCode(info.Code)
+	assert.Nil(err)
+	assert.Nil(cinfo)
 
-			ainfo, err := store.GetByAccess(info.GetAccess())
-			So(err, ShouldBeNil)
-			So(ainfo.GetUserID(), ShouldEqual, info.GetUserID())
+	// "Test access token store"
+	info = &models.Token{
+		ClientID:        "1",
+		UserID:          "1_1",
+		RedirectURI:     "http://localhost/",
+		Scope:           "all",
+		Access:          "1_1_1",
+		AccessCreateAt:  time.Now(),
+		AccessExpiresIn: time.Second * 5,
+	}
+	err = store.Create(info)
+	assert.Nil(err)
+	ainfo, err := store.GetByAccess(info.GetAccess())
+	assert.Nil(err)
+	assert.Equal(ainfo.GetUserID(), info.GetUserID(), "they should be equal")
 
-			err = store.RemoveByAccess(info.GetAccess())
-			So(err, ShouldBeNil)
+	err = store.RemoveByAccess(info.GetAccess())
+	assert.Nil(err)
 
-			ainfo, err = store.GetByAccess(info.GetAccess())
-			So(err, ShouldBeNil)
-			So(ainfo, ShouldBeNil)
-		})
+	ainfo, err = store.GetByAccess(info.GetAccess())
+	assert.Nil(err)
+	assert.Nil(ainfo)
 
-		Convey("Test refresh token store", func() {
-			info := &models.Token{
-				ClientID:         "1",
-				UserID:           "1_2",
-				RedirectURI:      "http://localhost/",
-				Scope:            "all",
-				Access:           "1_2_1",
-				AccessCreateAt:   time.Now(),
-				AccessExpiresIn:  time.Second * 5,
-				Refresh:          "1_2_2",
-				RefreshCreateAt:  time.Now(),
-				RefreshExpiresIn: time.Second * 15,
-			}
-			err := store.Create(info)
-			So(err, ShouldBeNil)
-
-			ainfo, err := store.GetByAccess(info.GetAccess())
-			So(err, ShouldBeNil)
-			So(ainfo.GetUserID(), ShouldEqual, info.GetUserID())
-
-			err = store.RemoveByAccess(info.GetAccess())
-			So(err, ShouldBeNil)
-
-			ainfo, err = store.GetByAccess(info.GetAccess())
-			So(err, ShouldBeNil)
-			So(ainfo, ShouldBeNil)
-
-			rinfo, err := store.GetByRefresh(info.GetRefresh())
-			So(err, ShouldBeNil)
-			So(rinfo.GetUserID(), ShouldEqual, info.GetUserID())
-
-			err = store.RemoveByRefresh(info.GetRefresh())
-			So(err, ShouldBeNil)
-
-			rinfo, err = store.GetByRefresh(info.GetRefresh())
-			So(err, ShouldBeNil)
-			So(rinfo, ShouldBeNil)
-		})
-	})
 }
