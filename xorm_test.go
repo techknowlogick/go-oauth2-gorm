f68c43d1953ff3a048deeb78905c866a47dea9a3
techknowlogick <hello@techknowlogick.com>
2018-11-12 23:33:07 -0500

    Update xorm_test.go

--- xorm_test.go
+++ xorm_test.go
@@ -1,1 +1,114 @@
 package oauth2xorm_test
+
+import (
+	"testing"
+	"time"
+
+	"gopkg.in/oauth2.v3/models"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	dsn = "root:@tcp(127.0.0.1:3306)/myapp_test?charset=utf8"
+)
+
+func TestTokenStore(t *testing.T) {
+	// wait gc
+	defer time.Sleep(time.Second * 2)
+
+	Convey("Test mysql token store", t, func() {
+		store := NewStore(NewConfig(dsn), "", 1)
+
+		Convey("Test authorization code store", func() {
+			info := &models.Token{
+				ClientID:      "1",
+				UserID:        "1_1",
+				RedirectURI:   "http://localhost/",
+				Scope:         "all",
+				Code:          "11_11_11",
+				CodeCreateAt:  time.Now(),
+				CodeExpiresIn: time.Second * 5,
+			}
+			err := store.Create(info)
+			So(err, ShouldBeNil)
+
+			cinfo, err := store.GetByCode(info.Code)
+			So(err, ShouldBeNil)
+			So(cinfo.GetUserID(), ShouldEqual, info.UserID)
+
+			err = store.RemoveByCode(info.Code)
+			So(err, ShouldBeNil)
+
+			cinfo, err = store.GetByCode(info.Code)
+			So(err, ShouldBeNil)
+			So(cinfo, ShouldBeNil)
+		})
+
+		Convey("Test access token store", func() {
+			info := &models.Token{
+				ClientID:        "1",
+				UserID:          "1_1",
+				RedirectURI:     "http://localhost/",
+				Scope:           "all",
+				Access:          "1_1_1",
+				AccessCreateAt:  time.Now(),
+				AccessExpiresIn: time.Second * 5,
+			}
+			err := store.Create(info)
+			So(err, ShouldBeNil)
+
+			ainfo, err := store.GetByAccess(info.GetAccess())
+			So(err, ShouldBeNil)
+			So(ainfo.GetUserID(), ShouldEqual, info.GetUserID())
+
+			err = store.RemoveByAccess(info.GetAccess())
+			So(err, ShouldBeNil)
+
+			ainfo, err = store.GetByAccess(info.GetAccess())
+			So(err, ShouldBeNil)
+			So(ainfo, ShouldBeNil)
+		})
+
+		Convey("Test refresh token store", func() {
+			info := &models.Token{
+				ClientID:         "1",
+				UserID:           "1_2",
+				RedirectURI:      "http://localhost/",
+				Scope:            "all",
+				Access:           "1_2_1",
+				AccessCreateAt:   time.Now(),
+				AccessExpiresIn:  time.Second * 5,
+				Refresh:          "1_2_2",
+				RefreshCreateAt:  time.Now(),
+				RefreshExpiresIn: time.Second * 15,
+			}
+			err := store.Create(info)
+			So(err, ShouldBeNil)
+
+			ainfo, err := store.GetByAccess(info.GetAccess())
+			So(err, ShouldBeNil)
+			So(ainfo.GetUserID(), ShouldEqual, info.GetUserID())
+
+			err = store.RemoveByAccess(info.GetAccess())
+			So(err, ShouldBeNil)
+
+			ainfo, err = store.GetByAccess(info.GetAccess())
+			So(err, ShouldBeNil)
+			So(ainfo, ShouldBeNil)
+
+			rinfo, err := store.GetByRefresh(info.GetRefresh())
+			So(err, ShouldBeNil)
+			So(rinfo.GetUserID(), ShouldEqual, info.GetUserID())
+
+			err = store.RemoveByRefresh(info.GetRefresh())
+			So(err, ShouldBeNil)
+
+			rinfo, err = store.GetByRefresh(info.GetRefresh())
+			So(err, ShouldBeNil)
+			So(rinfo, ShouldBeNil)
+		})
+	})
+}
